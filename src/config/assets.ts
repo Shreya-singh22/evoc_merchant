46f47da4a7d45b4d30354a1eb4eb0c9c53a15a9b
Shreua-singh22 <chauhanshreyasingh94@gmail.com>
2026-05-14 19:21:23 +0530

    fix: use NEXT_PUBLIC_BASE_PATH in workflow to correctly prefix image paths

--- src/config/assets.ts
+++ src/config/assets.ts
@@ -1,4 +1,4 @@
-export const BASE_PATH = process.env.NODE_ENV === 'production' ? '/evoc_merchant' : '';
+export const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 export const ASSETS = {
   reviews: [
